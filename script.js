a2206dd00873f5b1a323336f20243b0498f00580
aditya tadas <141910946+adityatadas24@users.noreply.github.com>
2023-10-01 11:33:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,51 @@
-function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+// function convertToRoman(num) {
+//   	const obj = {
+//       0:['M',1000], 
+//       1:['D', 500], 
+//       2:['C', 100], 
+//       3:['L', 50], 
+//       4:['X', 10], 
+//       5:['V', 5], 
+//       6:['I', 1]
+//     };
+     
+//   //your code here
 
-  //your code here
+// }
+function intToRoman(num) {
+  const romanNumerals = [
+    { value: 100000, numeral: 'C̅' },
+    { value: 90000, numeral: 'X̅C̅' },
+    { value: 50000, numeral: 'L̅' },
+    { value: 40000, numeral: 'X̅L̅' },
+    { value: 10000, numeral: 'X̅' },
+    { value: 9000, numeral: 'I̅X̅' },
+    { value: 5000, numeral: 'V̅' },
+    { value: 4000, numeral: 'I̅V̅' },
+    { value: 1000, numeral: 'M' },
+    { value: 900, numeral: 'CM' },
+    { value: 500, numeral: 'D' },
+    { value: 400, numeral: 'CD' },
+    { value: 100, numeral: 'C' },
+    { value: 90, numeral: 'XC' },
+    { value: 50, numeral: 'L' },
+    { value: 40, numeral: 'XL' },
+    { value: 10, numeral: 'X' },
+    { value: 9, numeral: 'IX' },
+    { value: 5, numeral: 'V' },
+    { value: 4, numeral: 'IV' },
+    { value: 1, numeral: 'I' },
+  ];
 
+  let result = '';
+  for (let i = 0; i < romanNumerals.length; i++) {
+    while (num >= romanNumerals[i].value) {
+      result += romanNumerals[i].numeral;
+      num -= romanNumerals[i].value;
+    }
+  }
+
+  return result;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
